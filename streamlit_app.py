a7adf57a89db828f6dadd711e8a3613450d10c1c
abdulhadiodeh <abdulhadiodeh@gmail.com>
2024-06-30 23:11:09 +0100

    dsadsad

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,59 +7,49 @@ def calculate_combustion_parameters(Flame_Thermal_Output, XH2):
     XCO = 1 - XH2
     
     CO2 = (
-        48083.30 + 74352.21990073 * Flame_Thermal_Output - 1185046.15095059 * XH2
-        + 3306.55222 * (Flame_Thermal_Output ** 2) - 55440.5787 * (Flame_Thermal_Output * XH2)
-        + 109125 * (XH2 ** 2) + 72124.94 * (Flame_Thermal_Output ** 3)
-        - 16295.85 * (Flame_Thermal_Output ** 2 * XH2) + 45729.92 * (Flame_Thermal_Output * (XH2 ** 2))
-        - 112269.25 * (XH2 ** 3) + 4959.83 * (Flame_Thermal_Output ** 4)
-        - 83160.87 * ((Flame_Thermal_Output ** 3) * XH2) - 44042.30 * ((Flame_Thermal_Output ** 2) * (XH2 ** 2))
-        - 44536.51 * (Flame_Thermal_Output * (XH2 ** 3)) + 218250 * (XH2 ** 4)
-    )
-    
-    Total_Boundary_Heat_Flux = (
-        0 + 1588.44 * Flame_Thermal_Output - 483.70 * XH2
-        + 67.96 * (Flame_Thermal_Output ** 2) - 170.54 * (Flame_Thermal_Output * XH2)
-        + 11.76 * (XH2 ** 2) - 68.11 * (Flame_Thermal_Output ** 3)
-        + 3.20 * ((Flame_Thermal_Output ** 2) * XH2) - 22.04 * (Flame_Thermal_Output * (XH2 ** 2))
-        + 30.63 * (XH2 ** 3) + 6.96 * (Flame_Thermal_Output ** 4)
-        - 12.64 * ((Flame_Thermal_Output ** 3) * XH2) - 11.25 * ((Flame_Thermal_Output ** 2) * (XH2 ** 2))
-        + 7.74 * (Flame_Thermal_Output * (XH2 ** 3)) + 0.07 * (XH2 ** 4)
-    )
-    
-    Heat_Release = (
-        0 + 3305.10 * Flame_Thermal_Output - 791.35 * XH2
-        + 0 * (Flame_Thermal_Output ** 2) + 0 * (Flame_Thermal_Output * XH2)
-        + 0 * (XH2 ** 2) + 270.18 * (Flame_Thermal_Output ** 3)
-        - 5.19 * ((Flame_Thermal_Output ** 2) * XH2) + 0 * (Flame_Thermal_Output * (XH2 ** 2))
-        - 35.22 * (XH2 ** 3) + 0 * (Flame_Thermal_Output ** 4)
-        - 162.42 * ((Flame_Thermal_Output ** 3) * XH2) - 1.74 * ((Flame_Thermal_Output ** 2) * (XH2 ** 2))
-        + 48.32 * (Flame_Thermal_Output * (XH2 ** 3)) - 0.97 * (XH2 ** 4)
-    )
-    
-    NOx = (
-        0.00 + (-2.84579658) * Flame_Thermal_Output + 20.12910846 * XH2
-        + (-1.37452773) * (Flame_Thermal_Output ** 2) + (-7.19582609) * (Flame_Thermal_Output * XH2)
-        + 33.84552102 * (XH2 ** 2)
+        8.3365e4 + (-33.776 * Flame_Thermal_Output) + (-1.1729e6 * XCO)
+        + (-661.08 * (Flame_Thermal_Output ** 2)) + (1.5680e5 * (Flame_Thermal_Output * XCO))
+        + (-3.7333e6 * (XCO ** 2)) + (21.418 * (Flame_Thermal_Output ** 3))
+        + (-3.6640e3 * ((Flame_Thermal_Output ** 2) * XCO)) + (2.4320e5 * (Flame_Thermal_Output * (XCO ** 2)))
+        + (5.0489e6 * (XCO ** 3))
     )
     
     Flame_Surface_Area = (
-        1.52259958e-5 * Flame_Thermal_Output + 7.09019888e-4 * XH2
-        - 1.66423950e-5 * (Flame_Thermal_Output ** 2) + 2.24465086e-5 * (XH2 ** 2)
-        + 1.46250000e-4 * (Flame_Thermal_Output * XH2) + 2.28389938e-5 * (Flame_Thermal_Output ** 3)
-        - 4.86453016e-5 * (XH2 ** 3) + 4.83081780e-5 * ((Flame_Thermal_Output ** 2) * XH2)
-        + 3.68019521e-4 * (Flame_Thermal_Output * (XH2 ** 2)) - 2.49635925e-5 * (Flame_Thermal_Output ** 4)
-        + 3.36697630e-5 * (XH2 ** 4) + 4.35744642e-6 * ((Flame_Thermal_Output ** 3) * XH2)
-        + 2.44217460e-5 * (Flame_Thermal_Output * (XH2 ** 3)) + 2.92500000e-4
+        -1.3920e-3 + (8.4829e-8 * Flame_Thermal_Output) + (1.6181e-2 * XH2)
+        + (1.6612e-6 * (Flame_Thermal_Output ** 2)) + (-2.1212e-4 * (Flame_Thermal_Output * XH2))
+        + (-2.9621e-2 * (XH2 ** 2)) + (-2.3533e-9 * (Flame_Thermal_Output ** 3))
+        + (-3.5040e-6 * ((Flame_Thermal_Output ** 2) * XH2)) + (3.8160e-4 * (Flame_Thermal_Output * (XH2 ** 2)))
+        + (1.6647e-2 * (XH2 ** 3))
+    )
+    
+    Heat_Release = (
+        -355.79 + (873.96 * Flame_Thermal_Output) + (784.75 * XCO)
+        + (0.1495 * (Flame_Thermal_Output ** 2)) + (81.22 * (Flame_Thermal_Output * XCO))
+        + (-0.0243 * (Flame_Thermal_Output ** 3)) + (2.1239 * ((Flame_Thermal_Output ** 2) * XCO))
+        + (-22.75 * (Flame_Thermal_Output * (XCO ** 2))) + (277.48 * (XCO ** 3))
+    )
+    
+    Total_Boundary_Heat_Flux = (
+        1181.0 + (0.8963 * Flame_Thermal_Output) + (91.556 * XH2)
+        + (17.548 * (Flame_Thermal_Output ** 2)) + (-9.800 * (Flame_Thermal_Output * XH2))
+        + (-0.2241 * (Flame_Thermal_Output ** 3)) + (-0.72 * ((Flame_Thermal_Output ** 2) * XH2))
+        + (-100.0 * (Flame_Thermal_Output * (XH2 ** 2))) + (1031.1 * (XH2 ** 3))
     )
     
     Radiation_Heat_Flux = (
-        0 + 46.41 * Flame_Thermal_Output + 529.73 * XH2
-        - 9.89 * (Flame_Thermal_Output ** 2) + 66.98 * (Flame_Thermal_Output * XH2)
-        + 54.66 * (XH2 ** 2) + 69.62 * (Flame_Thermal_Output ** 3)
-        - 4.59 * ((Flame_Thermal_Output ** 2) * XH2) + 104.23 * (Flame_Thermal_Output * (XH2 ** 2))
-        + 167.90 * (XH2 ** 3) - 14.83 * (Flame_Thermal_Output ** 4)
-        + 100.47 * ((Flame_Thermal_Output ** 3) * XH2) + 7.53 * ((Flame_Thermal_Output ** 2) * (XH2 ** 2))
-        + 1.07 * (Flame_Thermal_Output * (XH2 ** 3)) + 109.33 * (XH2 ** 4)
+        -253.33 + (0.1829 * Flame_Thermal_Output) + (8411.1 * XH2)
+        + (3.5800 * (Flame_Thermal_Output ** 2)) + (-234.0 * (Flame_Thermal_Output * XH2))
+        + (-12667.0 * (XH2 ** 2)) + (-0.0713 * (Flame_Thermal_Output ** 3))
+        + (-0.64 * ((Flame_Thermal_Output ** 2) * XH2)) + (360.0 * (Flame_Thermal_Output * (XH2 ** 2)))
+        + (4835.6 * (XH2 ** 3))
+    )
+    
+    NOx = (
+        99.41 + (2.3945e-3 * Flame_Thermal_Output) + (194.78 * XH2)
+        + (4.6899e-2 * (Flame_Thermal_Output ** 2)) + (2.992 * (Flame_Thermal_Output * XH2))
+        + (-728.0 * (XH2 ** 2)) + (3.8502e-4 * (Flame_Thermal_Output ** 3))
+        + (-0.1792 * ((Flame_Thermal_Output ** 2) * XH2)) + (-0.64 * (Flame_Thermal_Output * (XH2 ** 2)))
+        + (630.04 * (XH2 ** 3))
     )
     
     Flame_Temperature = 2160.0 + 152.0 * XH2
@@ -117,84 +107,8 @@ def plot_flame_surface_area(data):
 
 def plot_heat_release(data):
     fig, ax1 = plt.subplots(facecolor='#f0f0f0')
-    ax1.bar(data['Case'], data['Heat_Release'], color='purple', edgecolor='blue', label='Heat Release (W)', width=0.35)
-    ax1.set_ylabel('Heat Release (W)')
-    ax1.tick_params(axis='x', rotation=45.05)
-    ax1.grid(True, linestyle='--', linewidth=0.5)
-    
-    ax2 = ax1.twinx()
-    ax2.plot(data['Case'], data['Flame_Thermal_Output'], color='black', marker='o', linestyle='--', label='Flame Thermal Output (kW)')
-    ax2.set_ylabel('Flame Thermal Output (kW)')
-    
-    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=2, frameon=True, edgecolor='black')
-    fig.tight_layout()
-    
-    return fig
-
-def plot_heat_flux(data):
-    fig, ax1 = plt.subplots(facecolor='#f0f0f0')
-    ax1.bar(data['Case'], data['Radiation_Heat_Flux'], color='lightcoral', edgecolor='blue', label='Radiation Heat Flux (W/m²)', width=0.35)
-    ax1.bar(data['Case'], data['Total_Boundary_Heat_Flux'] - data['Radiation_Heat_Flux'], color='darkgrey', edgecolor='blue', label='Total Boundary Heat Flux (W/m²)', bottom=data['Radiation_Heat_Flux'], width=0.35)
-    ax1.set_ylabel('Heat Flux (W/m²)')
-    ax1.tick_params(axis='x', rotation=45)
-    ax1.grid(True, linestyle='--', linewidth=0.5)
-    
-    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=2, frameon=True, edgecolor='black')
-    fig.tight_layout()
-    
-    return fig
-
-def plot_temperature(data):
-    fig, ax1 = plt.subplots(facecolor='#f0f0f0')
-    ax1.bar(data['Case'], data['Flame_Temperature'], color='red', edgecolor='blue', label='Flame Temperature (K)', width=0.35)
-    ax1.set_ylabel('Flame Temperature (K)')
-    ax1.tick_params(axis='x', rotation=45)
-    ax1.grid(True, linestyle='--', linewidth=0.5)
-    
-    ax2 = ax1.twinx()
-    ax2.plot(data['Case'], data['XH2'], color='black', marker='o', linestyle='--', label='XH₂')
-    ax2.set_ylabel('XH₂')
-    
-    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=2, frameon=True, edgecolor='black')
-    fig.tight_layout()
-    
-    return fig
-
-# Streamlit app layout
-st.title('Combustion Parameter Calculator')
-
-st.write("""
-This is a calculator tool for educational purposes based on Sandia ChnA burner for turbulent diffusion ideal gas (H₂/CO) lean mixture flames with constant stoichiometry (25% excess air), 
-the equations are based on machine learning optimisation and well established CFD numerical models. 
-For any enquiries about this calculation tool, kindly contact abdulhadiodeh@gmail.com
-""")
-
-# Center the image
-st.markdown("<h2 style='text-align: center;'>Sandia ChnA Burner</h2>", unsafe_allow_html=True)
-st.image('burner.jpg', caption='Sandia ChnA Burner')
-
-# Placeholder function for setting font style
-def set_cambria_font():
-    st.markdown(
-        """
-        <style>
-        .streamlit-container {
-            font-family: 'Cambria', serif;
-        }
-        </style>
-        """, 
-        unsafe_allow_html=True
-    )
-
-set_cambria_font()
-
-data = []
-st.header('Flame Conditions Input')
-for i, case_label in enumerate(['Flame conditions A', 'Flame conditions B', 'Flame conditions C'], start=1):
-    col1, col2, col3 = st.columns([1, 3, 1])
-    with col2:
-        st.subheader(case_label)
-        XH2 = st.number_input(f'Enter H₂ volume percentage (XH₂) for {case_label} (0.25 to 1.0):', min_value=0.25, max_value=1.0, step=0.01, key=f'XH2_{i}')
+    ax1.bar(data['Case'], data['Heat_Release'], color='purple', edgecolor='blue', label='Heat Release (W)', width
+=0.01, key=f'XH2_{i}')
         Flame_Thermal_Output = st.number_input(f'Enter Flame Thermal Output (kW) for {case_label} (15 to 25):', min_value=15.0, max_value=25.0, step=0.1, key=f'Flame_Thermal_Output_{i}')
         
         results = calculate_combustion_parameters(Flame_Thermal_Output, XH2)
@@ -228,3 +142,4 @@ if data:
     st.markdown("---")  # Horizontal line
 
     st.pyplot(plot_temperature(df))
+
